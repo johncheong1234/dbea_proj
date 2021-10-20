2529288cbdd2e950fbe051f0b01985d8c05174cd
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-10-20 14:35:47 +0800

    deposit account post request

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import requests, json
 from functions import url
 
@@ -34,6 +34,46 @@ def getAllProductTypes():
     print(product)
 
     return {'Name_list':Name_List, 'ID_list':ID_List}
+
+@app.route("/deposit_account", methods = ['GET','POST'])
+def getDepositAccountDetails():
+    #Header
+    data = json.loads(request.data)
+    userID = data['userID']
+    PIN = data['PIN']
+    serviceName = 'getDepositAccountDetails'
+    OTP = '999999'
+    #Content
+    accountID = '7681'
+    
+    headerObj = {
+                        'Header': {
+                        'serviceName': serviceName,
+                        'userID': userID,
+                        'PIN': PIN,
+                        'OTP': OTP
+                        }
+                        }
+    contentObj = {
+                        'Content': {
+                        'accountID': accountID
+                        }
+                        }
+    final_url="{0}?Header={1}&Content={2}".format(url(),json.dumps(headerObj),json.dumps(contentObj))
+    response = requests.post(final_url)
+    serviceRespHeader = response.json()['Content']['ServiceResponse']['ServiceRespHeader']
+    errorCode = serviceRespHeader['GlobalErrorID']
+
+    if errorCode == '010000':
+        depositAccount = response.json()['Content']['ServiceResponse']['DepositAccount']
+        return depositAccount
+    elif errorCode == '010041':
+        return "OTP has expired.\nYou will receiving a SMS"
+    else:
+        return serviceRespHeader['ErrorText']
+    
+    
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8002, debug=True)
